97875b0cf567a5c77299cb66b616927679fda409
Luccas Barros <luccasbarros@pm.me>
2021-09-06 20:20:20 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import { logger } from "./logger";
+
+export default class Routes {
+  io;
+  constructor() {}
+
+  setSocketInstance(io) {
+    this.io = io;
+  }
+
+  async defaultRoute(request, response) {
+    response.end("hello world");
+  }
+
+  async options(request, response) {
+    response.writeHead(204);
+    response.end("hello world");
+  }
+
+  async post(request, response) {
+    logger.info("ae");
+    response.end();
+  }
+
+  async get(request, response) {
+    logger.info("ae");
+    response.end();
+  }
+
+  handler(request, response) {
+    response.end("hello world");
+  }
+}
---
+++ test/example.test.js
@@ -0,0 +1,7 @@
+import { describe, test, expect } from "@jest/globals";
+
+describe("Testando 123", () => {
+  test("test test", () => {
+    expect(true).toBeTruthy();
+  });
+});
